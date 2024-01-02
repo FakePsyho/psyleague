9458a3dd7fdaac9b3edc7d8a5c1272ace2ea4a17
Psyho <closedeyez@gmail.com>
2024-01-02 02:22:29 +0100

    bumped version to 0.3.0

--- psyleague/psyleague.py
+++ psyleague/psyleague.py
@@ -32,7 +32,7 @@
 
 
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 import signal
 import time
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 setup(
     name = 'psyleague',
